3b3a537481cbd99d235d0583a6063f84cf68c7e4
Payoshnee Joshi <payoshneejoshi@gmail.com>
2026-08-31 20:05:13 +0530

    [997] [Time Beats: 37.80%] [Memory Beats: 32.51%] - LeetPush

--- 997-Find-the-Town-Judge.cpp
+++ 997-Find-the-Town-Judge.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> outDegree(n+1,0);
-        vector<int> inDegree(n+1,0);
-
+        vector<int> degree(n+1,0);
+     
         for(auto it : trust){
-            outDegree[it[0]]++;
-            inDegree[it[1]]++;
+            degree[it[0]]--;
+            degree[it[1]]++;
         }
         for(int i = 1; i <= n; i++){
-            if(outDegree[i] == 0 && inDegree[i] == n - 1){
+            if(degree[i] == n - 1){
                 return i;
             }
         }
